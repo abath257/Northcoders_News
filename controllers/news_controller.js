1745b8ac474e4e592c80ef99314479a3ce9cf10e
abath257 <abath257@gmail.com>
2022-05-16 14:31:51 +0100

    Removed console.log, altered variable name, removed postgres

--- controllers/news_controller.js
+++ controllers/news_controller.js
@@ -3,7 +3,6 @@ const { fetchTopics } = require("../models/news_model.js");
 exports.getTopics = (req, res) => {
   fetchTopics()
     .then((topics) => {
-      console.log(topics);
       res.status(200).send({ topics });
     })
     .catch((err) => {
--- models/news_model.js
+++ models/news_model.js
@@ -3,7 +3,7 @@ const db = require("../db/index.js");
 
 exports.fetchTopics = (()=>{
   return db.query('SELECT * FROM topics;')
-  .then((topics) =>{
-    return topics.rows
+  .then((data) =>{
+   return data.rows
   })
  })
