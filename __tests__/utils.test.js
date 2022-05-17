46fb654c44069e1a73cddf5d5d6f648be4a454d7
abath257 <abath257@gmail.com>
2022-05-17 10:50:48 +0100

    Removed .only in utils file

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -4,7 +4,7 @@ const {
   formatComments,
 } = require("../db/helpers/utils");
 
-describe.only("convertTimestampToDate", () => {
+describe("convertTimestampToDate", () => {
   test("returns a new object", () => {
     const timestamp = 1557572706232;
     const input = { created_at: timestamp };
