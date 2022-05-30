84f51781c9b071019ce20011cb4dd0920923bbd8
abath257 <abath257@gmail.com>
2022-05-30 16:55:48 +0100

    reordered connection file

--- db/index.js
+++ db/index.js
@@ -4,12 +4,14 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
-
 const config =
   ENV === "production"?{connectionString:process.env.DATABASE_URL,
   ssl: {rejectUnauthorized : false}}
   : {}
+  
+if (!process.env.PGDATABASE) {
+  throw new Error('PGDATABASE not set');
+}
+
+
 module.exports = new Pool(config);
