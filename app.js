fc89c1dac317804e81ae8dce32664fa57bf2acbf
abath257 <abath257@gmail.com>
2022-06-14 10:48:09 +0100

    Added cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // dependency imports
+const cors = require('cors')
+app.use(cors());
 const express = require("express");
 const app = express();
+
 app.use(express.json());
 const apiRouter = require("./routes/api.router");
 
--- listen.js
+++ listen.js
@@ -1,8 +1,8 @@
 const app = require('./app')
 
 
-const PORT = process.env.PORT || 9090;  
+const PORT = process.env.PORT || 8080;  
 
-app.listen(PORT), ()=>{
+app.listen(PORT), ((err)=>{ if (err) throw err
   console.log(`listening on port ${PORT}`)
-}
+})
